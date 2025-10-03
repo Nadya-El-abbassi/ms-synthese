36665d723e208e6f7003c203c80f50c350b2744f
nadya <nouranadian@gmail.com>
2025-10-03 22:25:15 +0100

    first commit

---
+++ invetery-service/src/main/java/org/sid/inveteryservice/InveteryServiceApplication.java
@@ -0,0 +1,13 @@
+package org.sid.inveteryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InveteryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InveteryServiceApplication.class, args);
+    }
+
+}
---
+++ invetery-service/src/test/java/org/sid/inveteryservice/InveteryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.inveteryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InveteryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
